dccc34c3927154bdae36d39511ef6f2ffd3465fe
Adrian Jobda <adi.jobda@gmail.com>
2025-08-29 18:18:13 +0200

    add config for pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
@@ -7,6 +8,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
 };
 
 export default nextConfig;
